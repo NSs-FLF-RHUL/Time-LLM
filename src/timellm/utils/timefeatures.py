ce50678420b8811d986d8dc97743324bc840951e
jberteaud <joanna.berteaud@gmail.com>
2026-08-05 18:36:26 +0100

    Simplifying second datasets

--- src/timellm/utils/timefeatures.py
+++ src/timellm/utils/timefeatures.py
@@ -113,8 +113,6 @@ def time_features_from_frequency_str(freq_str: str) -> List[TimeFeature]:
             SecondOfMinute,
             MinuteOfHour,
             HourOfDay,
-            DayOfWeek,
-            DayOfMonth,
             DayOfYear,
         ],
     }
